93a37e2d24af80753cf74a03d6d625c9850785ae
CHPN <jacobitebeerlove@yahoo.co.jp>
2018-07-22 03:59:42 +0900

    update: windows home dir setting

--- .emacs.d/conf/windows-conf.el
+++ .emacs.d/conf/windows-conf.el
@@ -1,3 +1,5 @@
 (when (eq window-system 'w32)
   (set-file-name-coding-system 'cp932)
-  (setq locale-coding-system 'cp932))
+  (setq locale-coding-system 'cp932)
+  (setq default-directory (concat (getenv "HOMEPATH") "/"))
+  (setq command-line-default-directory (concat (getenv "HOMEPATH") "/")))

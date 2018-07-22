80d32026f11edfbcd06413db52beea7c3d3bd7e1
CHPN <jacobitebeerlove@yahoo.co.jp>
2018-07-22 15:16:50 +0900

    update: font and theme settings

--- .emacs.d/conf/windows-conf.el
+++ .emacs.d/conf/windows-conf.el
@@ -1,5 +1,13 @@
 (when (eq window-system 'w32)
+  ;; file name and message encoding
   (set-file-name-coding-system 'cp932)
   (setq locale-coding-system 'cp932)
+
+  ;; default path setting
   (setq default-directory (concat (getenv "HOMEPATH") "/"))
-  (setq command-line-default-directory (concat (getenv "HOMEPATH") "/")))
+  (setq command-line-default-directory (concat (getenv "HOMEPATH") "/"))
+
+  ;; font (face) setting
+  (set-face-attribute 'default nil
+                      :family "Ricty Diminished"
+                      :height 150))

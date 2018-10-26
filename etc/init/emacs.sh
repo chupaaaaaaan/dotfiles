425686d21386b5780dcd9760e062d243a3317f98
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2018-10-26 13:51:58 +0900

    Add git repolist

--- etc/init/emacs.sh
+++ etc/init/emacs.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 
+EMACSD="../../.emacs.d"
+
 apt-get -y install emacs25-nox
+
+# githubリポジトリのファイルをダウンロードする
+cd ${EMACSD}/public_repos
+cat ../conf/package.list | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#' | xargs -n1 -I@ curl -O @
+cd -
+

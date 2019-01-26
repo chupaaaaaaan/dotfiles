09829b52631850e11c91a4aef964d8974a8c7e0f
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-01-26 20:49:52 +0900

    いずれのディレクトリからでもdeploy.shが実行できるよう変更

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+cd $(dirname $0)
 for file in .??*
 do
     [ "${file}" = ".gitignore" ] && continue
     [ "${file}" = ".git" ]       && continue
 
-    ln -f -s `pwd`/${file} ~/${file}
+    ln -n -f -s `pwd`/${file} ~/${file}
 done
 

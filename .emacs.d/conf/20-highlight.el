085c55f5fac8d5c07f7453e80950c3efc37098f1
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-02-09 05:32:47 +0900

    emacs26用にハイライト設定修正、helm設定追加

--- .emacs.d/conf/20-highlight.el
+++ .emacs.d/conf/20-highlight.el
@@ -18,8 +18,8 @@
 (show-paren-mode t)
 (setq show-paren-delay 0)
 (setq show-paren-style 'expression)
-(set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "yellow")
+(set-face-background 'show-paren-match nil)
+(set-face-underline 'show-paren-match "yellow")
 
 ;; volatile-highlights
 (when (require 'volatile-highlights nil t)

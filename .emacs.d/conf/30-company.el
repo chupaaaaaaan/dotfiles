8171724571a7aa88c2265057061cd1d65c3eb784
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-02-12 01:05:42 +0000

    Add company settings.

---
+++ .emacs.d/conf/30-company.el
@@ -0,0 +1,7 @@
+;;; company
+(when (require 'auto-complete-config nil t)
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+)

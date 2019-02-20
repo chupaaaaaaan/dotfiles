c6d34fc3a765894008482fdf5d4360eb3ad3d024
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-02-20 22:12:00 +0900

    stack用パスを削除

--- .bash_profile
+++ .bash_profile
@@ -9,7 +9,7 @@ if [ -n "$BASH_VERSION" ]; then
 fi
 
 # set PATH so it includes user's private bin directories
-PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+PATH="$HOME/bin:$PATH"
 
 # set local profiles if they exist
 for f in $HOME/.bash_profile.d/*; do
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -401,7 +401,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (powerline total-lines diminish smart-mode-line ac-haskell-process flycheck-haskell flycheck undo-tree yaml-mode volatile-highlights vbasense helm haskell-mode init-loader egg auto-complete))))
+    (flymake-shell powerline total-lines diminish smart-mode-line ac-haskell-process flycheck-haskell flycheck undo-tree yaml-mode volatile-highlights vbasense helm haskell-mode init-loader egg auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

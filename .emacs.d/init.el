7af2446d5ef0f39a033d14a109ca6bbff0abac90
Takayuki Uchida <jacobitebeerlove@gmail.com>
2019-10-20 12:09:47 +0900

    Reference使ってないので削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -525,8 +525,8 @@
    '(
      ("t" "Write down the thoughts of this moment with a timestamp."      item  (file+headline ladicle/get-today-diary "Log")  "%(ladicle/org-get-time) %?\n"            :prepend 1)
      ("m" "Memorize something in the memo section of today's diary."      entry (file+headline ladicle/get-today-diary "Memo") "* %?\n"                                  :empty-lines 1 :jump-to-captured 1 :unnarrowed 1)
-     ("i" "Create a general task to the inbox and jump to the task file." entry (file+headline task-file "Inbox")              "* TODO %?\n  %U\n\n** Reference\n%i\n"   :empty-lines 1 :jump-to-captured 1)
-     ("p" "Create an interrupt task to the inbox and start clocking."     entry (file+headline task-file "Inbox")              "* TODO %?\n  %U\n\n** Reference\n%i\n"   :empty-lines 1 :clock-in 1 :clock-resume 1)
+     ("i" "Create a general task to the inbox and jump to the task file." entry (file+headline task-file "Inbox")              "* TODO %?\n  %U\n%i\n"                   :empty-lines 1 :jump-to-captured 1)
+     ("p" "Create an interrupt task to the inbox and start clocking."     entry (file+headline task-file "Inbox")              "* TODO %?\n  %U\n%i\n"                   :empty-lines 1 :clock-in 1 :clock-resume 1)
      ("s" "Add an event to the calendar."                                 entry (file+headline schedule-file "Schedule")       "* %?\n  SCHEDULED: <%(org-read-date)>\n" :empty-lines 1)
      ("h" "Collect hacking Emacs ideas!"                                  item  (file+headline task-file "Hacking Emacs")      "[ ] %?"                                  :prepend 1)
      ("w" "Wish list for my life!"                                        item  (file+headline mylist-file "My Wishes")        "* TODO %?"                               :prepend 1)

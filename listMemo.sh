d5d5297a081f9f05d43d89d620ee4d637e93ac10
Takayuki UCHIDA <uchida.takayuki@tis.co.jp>
2020-03-31 13:08:28 +0900

    formatted.

--- listMemo.sh
+++ listMemo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd ${HOME}
-grep -H "^\*\* .*$" Dropbox/org/diary/*.org | sed "s/.org:\*\* /@/g" | sed "s/[[:space:]]\+:MEMO://g" | cut -c19- | awk 'BEGIN{FS="@";OFS=" | ";a=1}{printf("% 5d | %s | %s\n",a,$1,$2); a=a+1}'
+grep -H "^\*\* .*$" Dropbox/org/diary/*.org |
+    sed "s/.org:\*\* /@/g" |
+    cut -c19- |
+    awk 'BEGIN{FS="@";OFS=" | ";a=1}{printf("% 5d | %s | %s\n",a,$1,$2); a=a+1}'

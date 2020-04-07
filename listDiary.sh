3c7084615431e5d774bba65567dd058e9ac4e619
Takayuki UCHIDA <uchida.takayuki@tis.co.jp>
2020-04-07 15:44:44 +0900

    日記の表示フォーマットを変更

--- listDiary.sh
+++ listDiary.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 cd ${HOME}
-grep -H "<..:..>" Dropbox/org/diary/*.org | sed -e "s/.org:  - </@/g" -e "s/\(:..\)> /\1@/g" | cut -c19- | awk 'BEGIN{FS="@";OFS=" | "}{print $1,$2,$3}'
+grep -H "<..:..>" Dropbox/org/diary/*.org |
+    sed -e "s/.org:  - </@/g" -e "s/\(:..\)> /\1@/g" |
+    cut -c19- |
+    awk 'BEGIN{
+           FS="@";
+           OFS=" | ";
+           a="x"
+         }
+         {
+           if(a!="x"$1){
+             print "\n--< "$1" >""-------------------------------------------------"
+           };
+           print $2,$3;
+           a="x"$1
+         }'
 

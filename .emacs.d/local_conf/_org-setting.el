9d660038fd68e2cf101369fa793825a80f6cda07
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2020-05-21 15:48:00 +0900

    割り込みタスクをすぐDONEに変更。

--- .emacs.d/local_conf/_org-setting.el
+++ .emacs.d/local_conf/_org-setting.el
@@ -9,7 +9,7 @@
       '(("tweet"      "セルフツイート" item  (file+headline ladicle/get-today-diary "Log")    "%(ladicle/org-get-time) %?\n" :prepend nil)
         ("memo"       "雑多なメモ"     entry (file+headline ladicle/get-today-diary "Memo")   "* %? :MEMO:\n"                :empty-lines 1 :jump-to-captured 1 :unnarrowed nil)
         ("inbox"      "タスクの作成"   entry (file+headline inbox-file "Inbox")               "* TODO %?\n  %U\n"            :empty-lines 1 :jump-to-captured nil)
-        ("interrupt"  "割り込みタスク" entry (file+headline inbox-file "Inbox")               "* TODO %?\n  %U\n"            :empty-lines 1 :clock-in 1 :clock-resume 1)
+        ("interrupt"  "割り込みタスク" entry (file+headline inbox-file "Inbox")               "* DONE %?\n  %U\n"            :empty-lines 1 :clock-in 1 :clock-resume 1)
         ("schedule"   "カレンダー"     entry (file+headline schedule-file "Schedule")         "* %?\n  SCHEDULED: <%(org-read-date)>\n  %U\n" :empty-lines 1)
         ("hack"       "ハックしたい事" item  (file+headline org-default-notes-file "Hacking") "[ ] %?"                       :prepend 1)
         ("link"       "リンクを追加"   item  (clock)                                          "%A\n"                         :immediate-finish 1 :prepend nil)))

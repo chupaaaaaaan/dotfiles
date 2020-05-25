ddffb574ba761c8c1bfabd35e2e0677fbd51435d
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-05-26 01:01:50 +0900

    capture時にPropertyを設定できるようにした

--- .emacs.d/local_conf/_org-setting.el
+++ .emacs.d/local_conf/_org-setting.el
@@ -5,17 +5,56 @@
                          ("develop" . ?d)
                          (:endgroup . nil)
                          ("chore" . ?c)))
+(setq sche "  SCHEDULED: <%(org-read-date)>\n")
+(setq scht "  SCHEDULED: <%(org-read-date t)>\n")
+(setq dead "  DEADLINE: <%(org-read-date)>\n")
+(setq pbgn "  :PROPERTIES:\n")
+(setq pend "  :END:\n")
+(setq peff "  :Effort: %(org-read-property-value \"Effort\")\n")
+
+(setq todo-entry     (concat "* TODO %?\n  %U\n" sche pbgn peff pend))
+(setq schedule-entry (concat "* %?\n  %U\n" scht))
+
 (setq my:org-capture-templates
-      '(("tweet"      "セルフツイート" item  (file+headline ladicle/get-today-diary "Log")    "%(ladicle/org-get-time) %?\n" :prepend nil)
-        ("memo"       "雑多なメモ"     entry (file+headline ladicle/get-today-diary "Memo")   "* %? :MEMO:\n"                :empty-lines 1 :jump-to-captured 1 :unnarrowed nil)
-        ("inbox"      "タスクの作成"   entry (file+headline inbox-file "Inbox")               "* TODO %?\n  %U\n"            :empty-lines 1 :jump-to-captured nil)
-        ("interrupt"  "割り込みタスク" entry (file+headline inbox-file "Inbox")               "* DONE %?\n  %U\n"            :empty-lines 1 :clock-in 1 :clock-resume 1)
-        ("schedule"   "カレンダー"     entry (file+headline schedule-file "Schedule")         "* %?\n  SCHEDULED: <%(org-read-date)>\n  %U\n" :empty-lines 1)
-        ("hack"       "ハックしたい事" item  (file+headline org-default-notes-file "Hacking") "[ ] %?"                       :prepend 1)
-        ("link"       "リンクを追加"   item  (clock)                                          "%A\n"                         :immediate-finish 1 :prepend nil)))
+      (list (list
+             "tweet" "セルフツイート" 'item
+             '(file+headline ladicle/get-today-diary "Log")
+             "%(ladicle/org-get-time) %?\n"
+             :prepend nil)
 
-;; ("daily-report"   "その日のまとめ"   entry (file+headline ladicle/get-today-diary "Memo") "* Report%?\n%(org-clock-report)\n"             :empty-lines 1)
-;; ("books-memo"     "読書メモ"         entry (file+headline ladicle/get-today-diary "Memo") "* %? :BOOK:\n"                                 :empty-lines 1 :jump-to-captured 1 :unnarrowed nil)
-;; ("wish-memo"      "欲しいものリスト" entry (file+headline inbox-file "My Wishes")         "* TODO %?"                                     :prepend 1)
+            (list
+             "memo" "雑多なメモ" 'entry
+             '(file+headline ladicle/get-today-diary "Memo")
+             "* %? :MEMO:\n"
+             :empty-lines 1 :jump-to-captured 1 :unnarrowed nil)
 
-;;
+            (list
+             "inbox" "タスクの作成" 'entry
+             '(file+headline inbox-file "Inbox")
+             todo-entry
+             :empty-lines 1 :jump-to-captured nil)
+
+            (list
+             "interrupt" "割り込みタスク" 'entry
+             '(file+headline inbox-file "Inbox")
+             "* TODO %?\n  %U\n"
+             :empty-lines 1 :clock-in 1 :clock-resume 1)
+
+            (list
+             "schedule" "カレンダー" 'entry
+             '(file+headline schedule-file "Schedule")
+             schedule-entry
+             :empty-lines 1)
+
+            (list
+             "hack-emacs" "Emacsをハック" 'checkitem
+             '(file+headline org-default-notes-file "Hacking Emacs")
+             "[ ] %?"
+             :prepend 1)
+
+            (list
+             "link" "リンクを追加" 'item
+             '(clock)
+             "%A\n"
+             :immediate-finish 1 :prepend nil)
+            ))

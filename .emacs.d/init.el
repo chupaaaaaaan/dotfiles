be70929b67c2397781a53806f13790f7bfe5f445
Takayuki Uchida <jacobitebeerlove@gmail.com>
2021-12-24 21:20:31 +0900

    haskell-modeのインデント修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1338,6 +1338,12 @@
 
 (use-package haskell-mode
   :ensure t
+  :custom
+  (haskell-indentation-layout-offset 4)
+  (haskell-indentation-left-offset 4)
+  (haskell-indentation-starter-offset 4)
+  (haskell-indentation-where-post-offset 4)
+  (haskell-indentation-where-pre-offset 4)
   :hook
   (haskell-mode . lsp)
   :bind

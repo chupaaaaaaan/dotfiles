32663dae26e6977ae903a2806b4f1cf3f0731379
Takayuki Uchida <jacobitebeerlove@gmail.com>
2022-04-09 01:45:34 +0900

    プロキシ設定を、デプロイ時に環境変数から設定する方法に変更

--- install/share/local-elisp.bash
+++ install/share/local-elisp.bash
@@ -2,4 +2,25 @@
 
 cd $(dirname $0)
 
-find ../../.emacs.d/local/ -type f | xargs -I@ cp -n @ ~/.elisp/local
+find ../../.emacs.d/local-elisp/ -type f | xargs -I@ cp -n @ ~/.elisp/local
+
+# プロキシ設定
+# HTTP_PROXY環境変数が存在している場合のみ、「scheme://」を削った文字列を追加
+# HTTP_PROXYおよびHTTPS_PROXYは同じになる想定
+PROXY_HOST="${HTTP_PROXY:+${HTTP_PROXY##*://}}"
+
+[ -n "${PROXY_HOST}" ] && cat <<EOF > ~/.elisp/local/local-proxy-conf.el 
+;;; local-proxy-conf --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'url-vars)
+
+(customize-set-variable 'url-proxy-services '(("http" . "${PROXY_HOST}")
+                                              ("https" . "${PROXY_HOST}")))
+
+(provide 'local-proxy-conf)
+;;; local-proxy-conf.el ends here
+EOF

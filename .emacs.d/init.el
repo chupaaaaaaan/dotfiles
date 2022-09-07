3186eddd60f31ff9737773fec4c748760ea98719
Takayuki Uchida <jacobitebeerlove@gmail.com>
2022-09-08 01:27:27 +0900

    org-captureのmemoをタイトルごとにディレクトリ分割

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1072,7 +1072,9 @@ https://github.com/ema2159/centaur-tabs#my-personal-configuration"
 
   (defun chpn/insert-today-string     () (format-time-string "%F"    (current-time)))
   (defun chpn/insert-timestamp-string () (format-time-string "%F %T" (current-time)))
-  (defun chpn/today-memo-string       () (concat org-directory memo-dir  (format-time-string "%F_" (current-time)) (read-string "memo title: ") ".org"))
+  (defun chpn/today-memo-string       () (let ((dn (concat org-directory memo-dir (format-time-string "%F_" (current-time)) (read-string "memo title: "))))
+                                            (unless (file-directory-p dn) (make-directory dn))
+                                            (concat dn "/index.org")))
   (defun chpn/today-issue-string      () (concat org-directory issue-dir (format-time-string "%F_" (current-time)) (read-string "issue title: ") ".org"))
   (defun ladicle/get-today-diary      () (concat org-directory diary-dir (format-time-string "%F.org" (current-time))))
   (defun ladicle/get-yesterday-diary  () (concat org-directory diary-dir (format-time-string "%F.org" (time-add (current-time) (* -24 3600)))))

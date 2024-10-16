ffc357dab3ec3a08d085d2c7435025da3a102b7d
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-10-17 07:21:49 +0900

    ディレクトリ作成などをdesktopリポジトリに移管

--- deploy.bash
+++ deploy.bash
@@ -2,24 +2,6 @@
 
 cd $(dirname $0)
 
-### create directories ###
-mkdir -p ~/.bash_profile.d
-mkdir -p ~/.bashrc.d
-mkdir -p ~/.local/bin
-mkdir -p ~/.local/share/tools
-mkdir -p ~/.local/share/applications
-mkdir -p ~/.local/share/bash-completion/completions
-mkdir -p ~/.local/share/fonts
-mkdir -p ~/bin
-
-# git clone https://github.com/git/git.git
-wget -q -O ~/.local/share/bash-completion/completions/git.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-wget -q -O ~/.local/share/tools/git-prompt.sh                  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-
-# git clone https://github.com/jonmosco/kube-ps1.git
-wget -q -O ~/.local/share/tools/kube-ps1.sh                    https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
-
-### deploy dotfiles ###
 for file in .??*
 do
     [ "${file}" = ".gitignore" ] && continue
@@ -28,4 +10,3 @@ do
     ln -n -f -s $(pwd)/${file} ~/${file}
     
 done
-

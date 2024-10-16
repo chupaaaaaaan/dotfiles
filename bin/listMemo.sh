3d5989c0028c6b4a2966a1a877726ff7494db451
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-10-17 07:22:12 +0900

    不要なファイルを削除

--- bin/listMemo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ${HOME}
-grep -H "^\*\* .*$" Dropbox/org/diary/*.org |
-    sed "s/.org:\*\* /@/g" |
-    cut -c19- |
-    awk 'BEGIN{FS="@";OFS=" | ";a=1}{printf("% 5d | %s | %s\n",a,$1,$2); a=a+1}'
